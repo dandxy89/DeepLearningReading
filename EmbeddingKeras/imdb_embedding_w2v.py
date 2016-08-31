06bdedd6fcd5309d3c95bf9fd2dd008a89a38e63
dan.dixey <dan.dixey@gmail.com>
2016-08-31 18:14:24 +0100

    Updating Example for Keras 1.08

--- EmbeddingKeras/imdb_embedding_w2v.py
+++ EmbeddingKeras/imdb_embedding_w2v.py
@@ -42,6 +42,7 @@
 
 import multiprocessing
 import numpy as np
+import os
 from gensim.models.word2vec import Word2Vec
 from gensim.corpora.dictionary import Dictionary
 
@@ -50,13 +51,18 @@ from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense, Dropout
-np.random.seed(1337)  # For Reproducibility
 
+# For Reproducibility
+np.random.seed(1337)
+
+# Changing the Working Directory - Using PyCharm
+#os.chdir("/Users/dan.dixey/Desktop/Personal/Github/DeepLearning_MachineLearning/EmbeddingKeras")
+#os.getcwd()
 
 # set parameters:
 vocab_dim = 300
 maxlen = 100
-n_iterations = 10  # ideally more..
+n_iterations = 10  # ideally more, since this improves the quality of the word vecs
 n_exposures = 30
 window_size = 7
 batch_size = 32
@@ -69,7 +75,7 @@ data_locations = {'./Data/test-neg.txt': 'TEST_NEG',
                   './Data/train-pos.txt': 'TRAIN_POS'}
 
 
-def import_tag(datasets=None):
+def import_tag(datasets = None):
     ''' Imports the datasets into one of two dictionaries
 
         Dicts:
@@ -108,9 +114,9 @@ def tokenizer(text):
     return text
 
 
-def create_dictionaries(train=None,
-                        test=None,
-                        model=None):
+def create_dictionaries(train = None,
+                        test = None,
+                        model = None):
     ''' Function does are number of Jobs:
         1- Creates a word to index mapping
         2- Creates a word to vector mapping
@@ -124,7 +130,7 @@ def create_dictionaries(train=None,
         w2indx = {v: k+1 for k, v in gensim_dict.items()}
         w2vec = {word: model[word] for word in w2indx.keys()}
 
-        def parse_dataset(data):
+        def parse_dataset(data ):
             ''' Words become integers
             '''
             for key in data.keys():
@@ -137,33 +143,33 @@ def create_dictionaries(train=None,
                         new_txt.append(0)
                 data[key] = new_txt
             return data
-        train = parse_dataset(train)
-        test = parse_dataset(test)
+        train = parse_dataset(train )
+        test = parse_dataset(test )
         return w2indx, w2vec, train, test
     else:
         print('No data provided...')
 
 
 print('Loading Data...')
-train, test = import_tag(datasets=data_locations)
+train, test = import_tag(datasets = data_locations)
 combined = train.values() + test.values()
 
 print('Tokenising...')
 combined = tokenizer(combined)
 
 print('Training a Word2vec model...')
-model = Word2Vec(size=vocab_dim,
-                 min_count=n_exposures,
-                 window=window_size,
-                 workers=cpu_count,
-                 iter=n_iterations)
+model = Word2Vec(size = vocab_dim,
+                 min_count = n_exposures,
+                 window = window_size,
+                 workers = cpu_count,
+                 iter = n_iterations)
 model.build_vocab(combined)
 model.train(combined)
 
 print('Transform the Data...')
-index_dict, word_vectors, train, test = create_dictionaries(train=train,
-                                                            test=test,
-                                                            model=model)
+index_dict, word_vectors, train, test = create_dictionaries(train = train,
+                                                            test = test,
+                                                            model = model)
 
 print('Setting up Arrays for Keras Embedding Layer...')
 n_symbols = len(index_dict) + 1  # adding 1 to account for 0th index
@@ -178,8 +184,8 @@ X_test = test.values()
 y_test = [1 if value > 12500 else 0 for value in test.keys()]
 
 print("Pad sequences (samples x time)")
-X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
-X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+X_train = sequence.pad_sequences(X_train, maxlen = maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen = maxlen)
 print('X_train shape:', X_train.shape)
 print('X_test shape:', X_test.shape)
 
@@ -187,29 +193,71 @@ print('Convert labels to Numpy Sets...')
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
-print('Defining a Simple Keras Model...')
-model = Sequential()  # or Graph or whatever
-model.add(Embedding(output_dim=vocab_dim,
-                    input_dim=n_symbols,
-                    mask_zero=True,
-                    weights=[embedding_weights],
-                    input_length=input_length))  # Adding Input Length
+print('Defining a Simple Sequential Keras Model...')
+model = Sequential()
+model.add(Embedding(output_dim = vocab_dim,
+                    input_dim = n_symbols,
+                    mask_zero = True,
+                    weights = [embedding_weights],
+                    input_length = input_length))
+
 model.add(LSTM(vocab_dim))
 model.add(Dropout(0.3))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation = 'sigmoid'))
+model.summary()
 
 print('Compiling the Model...')
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              class_mode='binary')
+model.compile(optimizer = 'rmsprop',
+              loss = 'binary_crossentropy',
+              metrics = ['accuracy'])
 
 print("Train...")
-model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=n_epoch,
-          validation_data=(X_test, y_test), show_accuracy=True)
+model.fit(X_train, y_train,
+          batch_size = batch_size,
+          nb_epoch = n_epoch,
+          validation_data = (X_test, y_test),
+          shuffle = True)
 
 print("Evaluate...")
-score, acc = model.evaluate(X_test, y_test,
-                            batch_size=batch_size,
-                            show_accuracy=True)
-print('Test score:', score)
-print('Test accuracy:', acc)
+score = model.evaluate(X_test, y_test,
+                       batch_size = batch_size)
+
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+# Output:
+
+# Using Theano backend.
+# Loading Data...
+# Tokenising...
+# Training a Word2vec model...
+# Transform the Data...
+# Setting up Arrays for Keras Embedding Layer...
+# Creating Datesets...
+# Pad sequences (samples x time)
+# ('X_train shape:', (25000, 100))
+# ('X_test shape:', (25000, 100))
+# Convert labels to Numpy Sets...
+# Defining a Simple Sequential Keras Model...
+# ____________________________________________________________________________________________________
+# Layer (type)                     Output Shape          Param #     Connected to
+# ====================================================================================================
+# embedding_1 (Embedding)          (None, 100, 300)      4610400     embedding_input_1[0][0]
+# ____________________________________________________________________________________________________
+# lstm_1 (LSTM)                    (None, 300)           721200      embedding_1[0][0]
+# ____________________________________________________________________________________________________
+# dropout_1 (Dropout)              (None, 300)           0           lstm_1[0][0]
+# ____________________________________________________________________________________________________
+# dense_1 (Dense)                  (None, 1)             301         dropout_1[0][0]
+# ====================================================================================================
+# Total params: 5331901
+# ____________________________________________________________________________________________________
+# Compiling the Model...
+# Train...
+# Train on 25000 samples, validate on 25000 samples
+# Epoch 1/2
+# 25000/25000 [==============================] - 430s - loss: 0.4413 - acc: 0.7951 - val_loss: 0.4435 - val_acc: 0.7843
+# Epoch 2/2
+# 25000/25000 [==============================] - 418s - loss: 0.2940 - acc: 0.8777 - val_loss: 0.3328 - val_acc: 0.8538
+# ('Test score:', 0.33278348482608794)
+# ('Test accuracy:', 0.8538)
